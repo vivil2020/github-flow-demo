3b4dfa4aaff3382495a6357522d53889397e1c65
max bay <2923895996@@qq.com>
2026-03-23 17:30:38 +0800

    修改了 a1 a2 函数

--- a.js
+++ a.js
@@ -1,6 +1,6 @@
 function a1 (...args) {
-  console.log("args", args);
+  console.log("args", args,"a1");
 }
 function a2 (...args) {
-  console.log("args", args);
+  console.log("args", args,"a2");
 }
